482dfb8b1e35b2b1d8d99fcee01ceeef1e44753a
Ishan <ishanganguly10a12@gmail.com>
2026-03-02 14:57:36 +0530

    Implemented Ex 10

--- SOLID/Ex9/src/SimpleConsole.java
+++ SOLID/Ex9/src/SimpleConsole.java
@@ -3,10 +3,4 @@ public class SimpleConsole {
     public void log(String s) {
         System.out.println(s);
     }
-}// Smell: unused console wrapper
-
-public class SimpleConsole {
-    public void log(String s) {
-        System.out.println(s);
-    }
 }
