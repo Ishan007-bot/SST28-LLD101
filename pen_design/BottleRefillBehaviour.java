021f0e8758b1434327021bdd08a3d63b77e60e87
Ishan <ishanganguly10a12@gmail.com>
2026-03-23 22:33:46 +0530

    Implemented a pen design

---
+++ pen_design/BottleRefillBehaviour.java
@@ -0,0 +1,6 @@
+public class BottleRefillBehaviour implements RefillBehaviour {
+    @Override
+    public void refill() {
+        System.out.println("Drawing ink from a bottle using a converter.");
+    }
+}
